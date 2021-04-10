fbcb94bfbbf69ca300264218e95fff74d84375f5
tonyMICCELI <tony.cuillandre@gmail.com>
2021-04-10 18:05:43 +0200

    Begin the setup for the index page

--- js/instantiate.js
+++ js/instantiate.js
@@ -1,11 +1,11 @@
 // Fichier JS pour instancier les articles du site
 
 //Création des différents articles
-var fraise = new Article("fraise", "5€", "1Kg", "../img/fraises.png");
-var baguette = new Article("baguette", "2€", "800g", "../img/pain.png", "cat-pain");
-var camembert = new Article("camembert", "1.5€", "200g", "../img/fromage.png", "cat-fromage");
-var pizzaFromage = new Article("pizza 4 fromages", "3€", "500g", "../img/pizza.png", "cat-pizza");
-var pinard = new Article("bon pinard", "3€", "70cl", "../img/vin.png", "cat-vin");
+var fraise = new Article("fraise", "5€", "1Kg", "img/fraises.png");
+var baguette = new Article("baguette", "2€", "800g", "img/pain.png", "cat-pain");
+var camembert = new Article("camembert", "1.5€", "200g", "img/fromage.png", "cat-fromage");
+var pizzaFromage = new Article("pizza 4 fromages", "3€", "500g", "img/pizza.png", "cat-pizza");
+var pinard = new Article("bon pinard", "3€", "70cl", "img/vin.png", "cat-vin");
 //Affichage des différents articles
 var articlesToAppend = [fraise,baguette,camembert,pizzaFromage,pinard];
 for (var i = 0; i < articlesToAppend.length; i++) {
